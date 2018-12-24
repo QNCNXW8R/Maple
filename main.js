83d27cb5fb7249cdd2c4496c405ab15c20cc7a28
QNCNXW8R <danielrcgray@gmail.com>
2018-12-24 18:40:18 +0000

    Logs xml document to console

--- main.js
+++ main.js
@@ -1,9 +1,5 @@
 // By Qnc
 
-//var data = document.getElementById('data').contentDocument;
-//var template = document.getElementById('template').contentDocument;
-//console.log(template.getElementsByTagName('equipment-list'));
-
 var xhttp;
 if(window.XMLHttpRequest){
     xhttp = new XMLHttpRequest();
@@ -12,7 +8,7 @@ if(window.XMLHttpRequest){
 }
 xhttp.onreadystatechange = function() {
     if (xhttp.readyState == 4 && xhttp.status == 200) {
-       alert(xhttp);
+       console.log(xhttp);
     }
 }
 
